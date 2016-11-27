1e2844c00bff53f8d73ce5edf5e84e0b30b62b7e
fumjum <pfrench@calstatela.edu>
2016-11-27 10:53:35 -0800

    Update server.js

--- CS3035/contactlistapp/server.js
+++ CS3035/contactlistapp/server.js
@@ -53,5 +53,5 @@ app.put('/contactlist/:id', function (req, res) {
   );
 });
 
-app.listen(3000);
-console.log("Server running on port 3000");
+app.listen(8080);
+console.log("Server running on port 8080");
